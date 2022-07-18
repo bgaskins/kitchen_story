44481292a7f33be7a5fc9ce930c19487dc1112b1
Bryan Gaskins <bryandgaskins@gmail.com>
2022-07-18 15:18:05 -0400

    Added store components, Bootstrap and JQuery

---
+++ src/app/add-remove/add-remove.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-remove',
+  templateUrl: './add-remove.component.html',
+  styleUrls: ['./add-remove.component.css']
+})
+export class AddRemoveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorComponent } from './error/error.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { AddRemoveComponent } from './add-remove/add-remove.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorComponent,
+    FooterComponent,
+    LoginComponent,
+    LogoutComponent,
+    MenuComponent,
+    HomeComponent,
+    SearchComponent,
+    OrderDetailsComponent,
+    AddRemoveComponent,
+    ConfirmationComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
