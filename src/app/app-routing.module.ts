82111514144dade4cd39911a0a18bbf32a3ec428
Bryan Gaskins <bryandgaskins@gmail.com>
2022-07-30 09:37:23 -0400

    Added route path for CartComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,9 @@ const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'welcome/:name', component: WelcomeComponent, canActivate: [RouteGuardService]},
+  { path: 'cart', component: CartComponent },
   { path: 'product-list', component: ProductListComponent, canActivate: [RouteGuardService]},
+  { path: '', component: ProductListComponent, canActivate: [RouteGuardService]},
   { path: 'logout', component: LogoutComponent },
   { path: '**', component: ErrorComponent },
 ];
